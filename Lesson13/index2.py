d71921bccf637ff88ed91322417314c8f15a0b30
H.ivy <168252139+Hivvyyy@users.noreply.github.com>
2024-06-29 03:15:53 +0000

    motify

--- Lesson13/index2.py
+++ Lesson13/index2.py
@@ -4,15 +4,21 @@ import streamlit as st
 import source
 from source import Root
 
-data_str = source.download_youbike()
-root = Root.model_validate_json(data_str)
-data = root.model_dump()
+try:
+    data_str = source.download_youbike()
 
-def ijk(value):
-    return value['行政區']
+except Exception as e:
+    st.error(e)
 
-areas:list[str] = list(set(map(ijk,data)))
+else:
+    root = Root.model_validate_json(data_str)
+    data = root.model_dump()
 
-option = st.selectbox("請選擇行政區",areas)
-st.write("您選擇:", option)
+    def ijk(value):
+        return value['行政區']
+
+    areas:list[str] = list(set(map(ijk,data)))
+
+    option = st.selectbox("請選擇行政區",areas)
+    st.write("您選擇:", option)
 
